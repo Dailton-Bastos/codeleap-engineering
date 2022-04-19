51c2742f0587745892898806cb39cb845efad80a
Dailton Bastos <dailtonbastos@gmail.com>
2022-04-18 22:57:52 -0300

    feat: add header page posts

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+export const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <h1>CodeLeap Network</h1>
+    </header>
+  )
+}
